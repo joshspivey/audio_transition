be656e97c9b6d79df68e5550df7ca7da2901748d
Josh Spivey <joshspivey@new-host.home>
2012-11-17 17:58:38 -0800

    fixed math

--- includes/js/audioSlider.js
+++ includes/js/audioSlider.js
@@ -8,6 +8,7 @@ AudioSlider.prototype =
     init: function()
     {
         var self = this;
+		this.sliderData = 0;
 		this.flashObj = document.getElementById('flashAudio');
         self.initSlider();
     },
@@ -22,22 +23,21 @@ AudioSlider.prototype =
         }).bind("slider:ready slider:changed",
         function(event, data)
         {
-			var reverseSound = (100 - data.value.toFixed(3)) / 100;
-			var forwardSound = data.value.toFixed(3)/100;
-			var forwardDrop = (reverseSound>0.60)?forwardSound / 1000 : forwardSound;
-			var reverseDrop = (reverseSound<0.40)?reverseSound / 1000 : reverseSound;	
-
-			self.flashObj.fadeSound(forwardDrop, reverseDrop);
-            $(this).nextAll(".output:first").html(data.value.toFixed(3));
+			self.sliderData = data.value.toFixed(3);
+			self.transistionMusic(self.sliderData);
+            $(this).nextAll(".output:first").html(self.sliderData);
         });
         $("#audioSlider-slider").simpleSlider("setValue", $("#audioSlider-slider").attr("data-default-value"));
 
-    }
+    },
+	transistionMusic: function(sliderData){
+		var self = this;
+		var reverseSound = (100 - sliderData) / 100;
+		var forwardSound = sliderData/100;
+		var forwardDrop = (reverseSound>0.60)?forwardSound / 1000 : forwardSound;
+		var reverseDrop = (reverseSound<0.40)?reverseSound / 1000 : reverseSound;	
+
+		self.flashObj.fadeSound(forwardDrop, reverseDrop);
+	}
 	
-
-
-
 };
-
-
-var aSlider = new AudioSlider();
